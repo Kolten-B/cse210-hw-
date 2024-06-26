723aeacd322161d92e944b86b682498aa5c7ba7f
Kolten-B <kendell282@gmail.com>
2024-06-26 16:30:54 -0600

    Finished Encapsulation

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction frac1 = new Fraction();
+        Fraction frac2 = new Fraction(6);
+        Fraction frac3 = new Fraction(3, 4);
+
+        Console.WriteLine(frac1.GetFractionString());
+        Console.WriteLine(frac1.GetDecimalValue());
+
+        Console.WriteLine(frac2.GetFractionString());
+        Console.WriteLine(frac2.GetDecimalValue());
+
+        Console.WriteLine(frac3.GetFractionString());
+        Console.WriteLine(frac3.GetDecimalValue());
+
+        frac1.Numerator = 5;
+        frac1.Denominator = 8;
+        Console.WriteLine(frac1.GetFractionString());
+        Console.WriteLine(frac1.GetDecimalValue());
+
+        frac2.Numerator = 2;
+        frac2.Denominator = 3;
+        Console.WriteLine(frac2.GetFractionString());
+        Console.WriteLine(frac2.GetDecimalValue());
+
+        frac3.Numerator = 7;
+        frac3.Denominator = 9;
+        Console.WriteLine(frac3.GetFractionString());
+        Console.WriteLine(frac3.GetDecimalValue());
     }
 }
